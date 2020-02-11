cf2d6229709fa236ac79c2d76cb90e18729d3593
nvisal1 <32280549+nvisal1@users.noreply.github.com>
2020-02-10 22:57:05 -0500

    remove react-pose from GameBoard.js

--- Tic-Tac-Toe/ClientApp/src/components/GameBoard/GameBoard.js
+++ Tic-Tac-Toe/ClientApp/src/components/GameBoard/GameBoard.js
@@ -1,29 +1,7 @@
 import React from 'react';
 import './GameBoard.css';
-import posed from 'react-pose';
-
-const DividerVerticalLeftAnimation = posed.div({
-  enter: {
-      y: 0,
-      opacity: 1,
-      delay: 300,
-      transition: {
-        y: { type: 'spring', stiffness: 300, damping: 12 },
-        default: { duration: 300 }
-      }
-  },
-  exit: {
-      y: 50,
-      opacity: 0,
-      transition: { duration: 150 }
-    }
-});
-
-let isVisible = false;
 
 export const GameBoard = ({ gameBoardTiles, isPlayerTurn, handleTileSelection }) => {
-
-  isVisible = true;
  
   return (
     <div className='game-board-container'>
@@ -31,9 +9,7 @@ export const GameBoard = ({ gameBoardTiles, isPlayerTurn, handleTileSelection })
         { renderTiles(gameBoardTiles, isPlayerTurn, handleTileSelection) }
       </div>
       <div className='game-board-container__dividers'>
-        {/* <DividerVerticalLeftAnimation pose={ isVisible ? 'enter' : 'exit'}> */}
         <div className='divider__vertical__left divider'></div>
-        {/* </DividerVerticalLeftAnimation> */}
         <div className='divider__vertical__right divider'></div>
         <div className='divider__horizontal__top divider'></div>
         <div className='divider__horizontal__bottom divider'></div>
